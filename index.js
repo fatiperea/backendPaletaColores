f3afdb383f156c4bf3bc607cbedd8f0a94449c2e
fatiperea <fatimaperea3pppp0@gmail.com>
2024-03-26 15:57:26 -0300

    completo middlewares

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-import express from 'express'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
 
-const app= express()
+const app = express();
 
-app.set('puerto', process.env.PORT || 4000)
+app.set("puerto", process.env.PORT || 4000);
 
-app.listen(app.get('puerto'),()=>{console.log("puerto", app.get('puerto'))})
+app.listen(app.get("puerto"), () => {
+  console.log("puerto", app.get("puerto"));
+});
+
+app.use(cors());
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+app.use(express.urlencoded())
+
+app.get("/", (req,res)=>{
+    console.log("solic. get")
+    res.send("respuesta del backend")
+})
